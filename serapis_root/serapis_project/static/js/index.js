0895cf59acb384e4c3df89169e816f7d8656fa73
Molly Geerling <mollygeerling@gmail.com>
2020-03-15 11:31:50 -0600

    slideshow fixes

--- serapis_root/serapis_project/static/js/index.js
+++ serapis_root/serapis_project/static/js/index.js
@@ -24,3 +24,57 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
+
+// photo slider
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("my-slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("my-slides");
+//   var dots = document.getElementsByClassName("dot");
+//
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//
+//   slideIndex++;
+//
+//   if (n > slides.length) {
+//     slideIndex = 1
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length
+//   }
+//
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace("active", "");
+//   }
+//
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += "active";
+// }

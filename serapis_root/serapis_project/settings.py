3f09e44dd7aa08a93508c1a4d1fe65fd42f89901
Molly Geerling <mollygeerling@gmail.com>
2020-07-20 05:46:20 -0600

    secret key mod

--- serapis_root/serapis_project/settings.py
+++ serapis_root/serapis_project/settings.py
@@ -26,7 +26,7 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'g0a$kal1c3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
